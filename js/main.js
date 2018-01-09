60dd71255f46697903a562112eaaccc08748a67c
mareyc <chao.mary@gmail.com>
2018-01-08 21:16:29 -0800

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "king of diamonds";
+var cardTwo = "king of hearts";
+var cardThree = "queen of diamonds";
+var cardFour = "queen of hearts";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
