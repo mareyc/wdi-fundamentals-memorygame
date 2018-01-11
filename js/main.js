8f744cdaa46196dbc31adc23d3b779978f305a00
mareyc <chao.mary@gmail.com>
2018-01-10 18:35:15 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
-console.log("Up and running!");
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert ("You found a match!");
-  }
+var checkForMatch = function () {
+  if (cardsInPlay.length === 2) {
+      if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert ("You found a match!");
+      }
+      else {
+        alert ("Sorry, try again");
+      }
+    }
   else {
-    alert ("Sorry, try again");
-  }
-}
-else {
-  console.log("Two cards must be selected!")
-}
+    console.log("Two cards must be selected!");
+    }
+};
+
+var flipCard = function (cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
