dc6d948924cf40941a9b5057bc0d4e440729170f
mareyc <chao.mary@gmail.com>
2018-01-11 20:57:19 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  },
+];
 var cardsInPlay = [];
 
 var checkForMatch = function () {
-  if (cardsInPlay.length === 2) {
+  //if (cardsInPlay.length === 2) {
       if (cardsInPlay[0] === cardsInPlay[1]) {
         alert ("You found a match!");
       }
       else {
         alert ("Sorry, try again");
       }
-    }
-  else {
-    console.log("Two cards must be selected!");
-    }
-};
+    };
+  // else {
+  //   console.log("Two cards must be selected!");
+  //   }
+//};
 
 var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
   checkForMatch();
 };
 
